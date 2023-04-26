48595edc14621b33e3ce1bda015fe368f8a9fe43
organtin <giovanni.organtini@roma1.infn.it>
2023-04-26 15:08:22 +0200

    a better version of the reader

--- Springer/physWArduinoSmartphones/cap-03/more/readArduino.py
+++ Springer/physWArduinoSmartphones/cap-03/more/readArduino.py
@@ -1,10 +1,34 @@
 # a better arduino reader (need pip3 install pyserial)
 
+'''
+    readArduino.py - a script to get data from an Arduino connected to a USB port
+    Copyright (C) 2023 by giovanni.organtini@uniroma1.it
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import serial
 import time
 
-usb = serial.Serial('/dev/cu.usbmodem14101')
-f = open('data.csv', 'w')
+port = '/dev/cu.usbmodem14201'
+filename = 'data.csv'
+
+print(f'Reading data from port {port}. Writing on {filename}')
+print(f'Ctrl+C to stop reading')
+
+usb = serial.Serial(port)
+f = open(filename, 'w')
 while True:
     try:
         arduinoReading = usb.readline().rstrip().decode()
